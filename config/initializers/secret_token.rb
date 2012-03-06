35526478ddea581530c84aa054c179e2561f6f83
Wishmaster <chooh@chooh.msk.ru>
2012-03-05 19:24:55 -0500

    First

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Whitespace::Application.config.secret_token = 'a5a3b22430c8dcd10b3a469fca5d55809466a9fedb57713c43eedc8bf4c68221d6a3bb9e3eafadbdcba3d910522af974e482e21e5474e4f35a9a0ed73f5d6120'
